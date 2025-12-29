73b2a3c2ffc15a71c6e386a812d9daeda2cfecff
Redbattlebot <redsawesomechannel@gmail.com>
2025-12-28 23:21:28 -0500

    Update surreal.ts

--- Site/src/lib/server/process/surreal.ts
+++ Site/src/lib/server/process/surreal.ts
@@ -1,39 +1,5 @@
-let started = false
-
+// Neutralized for SurrealDB Cloud
 export default async () => {
-	if (started) return
-	started = true
-	console.log("Starting SurrealDB...")
-
-	try {
-		const proc = Bun.spawn(
-			[
-				"surreal",
-				"start",
-				"-u=root",
-				"-p=root",
-				"surrealkv://data/surreal",
-			],
-			{ cwd: "..", stdout: "pipe", stderr: "pipe" }
-		)
-
-		process.on("exit", () => {
-			console.log("Shutting down SurrealDB...")
-			proc.kill()
-		})
-
-		proc.exited.then(async () => {
-			console.error("SurrealDB process exited unexpectedly.")
-			const r = await proc.stderr.getReader().read()
-			const logs = new TextDecoder().decode(r.value)
-			console.log(logs.split("\n").slice(-10).join("\n"))
-			process.exit(1)
-		})
-	} catch {
-		console.error(
-			"Failed to start SurrealDB. Please make sure it is installed and accessible as `surreal`."
-		)
-		console.error("https://docs.mercs.dev/install/surrealdb/")
-		process.exit(1)
-	}
+    // We no longer start a local process because we connect to the Cloud.
+    return;
 }
